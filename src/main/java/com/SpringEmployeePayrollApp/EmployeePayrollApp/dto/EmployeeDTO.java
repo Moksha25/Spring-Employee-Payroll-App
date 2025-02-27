3e405fce3c45605a03d18ff416261507b2c09f8b
Mokshini Baglekar <baglekarmokshini03@gmail.com>
2025-02-27 10:51:57 +0530

    [Mokshini Baglekar] Add: Use lombok library to auto generate getters and setters for the dto.

--- src/main/java/com/SpringEmployeePayrollApp/EmployeePayrollApp/dto/EmployeeDTO.java
+++ src/main/java/com/SpringEmployeePayrollApp/EmployeePayrollApp/dto/EmployeeDTO.java
@@ -1,21 +1,11 @@
 package com.SpringEmployeePayrollApp.EmployeePayrollApp.dto;
 
-public class EmployeeDTO {
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+public class EmployeeDTO {
     private String name;
     private double salary;
-
-    public EmployeeDTO() {}
-
-    public EmployeeDTO(String name, String gender, String department, double salary) {
-        this.name = name;
-        this.salary = salary;
-    }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-
-    public double getSalary() { return salary; }
-    public void setSalary(double salary) { this.salary = salary; }
 }
